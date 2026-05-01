8ac375fae7f0e5d6d0912eaa07a9762c3115cf54
YK <yk001g@gmail.com>
2026-05-01 10:12:54 +0900

    Remove base config for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yk001g.github.io',
-  base: '/yk_memo/',
+  site: 'https://yk-memo.vercel.app',
 });
